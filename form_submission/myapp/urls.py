e86bdaaaf64c605d7e5833c20c46ac511a1c652d
roychandu <cr717753@gmail.com>
2024-08-26 07:55:51 +0530

    update urls.py

--- form_submission/myapp/urls.py
+++ form_submission/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path("", views.show_index("indexpage"))
+    path("", views.showindex, name="indexpage")
 ]
--- form_submission/myapp/views.py
+++ form_submission/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def show_index(request):
-    return render(request, "myapp/index.html")
+def showindex(request):
+    return render(request, "app/index.html")
