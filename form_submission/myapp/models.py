acbe133653cacdb66542deee56e687d2b307a8c2
roychandu <cr717753@gmail.com>
2024-08-27 22:24:59 +0530

    create model for database

--- form_submission/myapp/models.py
+++ form_submission/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class FormData(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    contact = models.BigIntegerField()
+    address1_field = models.CharField(max_length=50)
+    address2_field = models.CharField(max_length=50)
+    state_field = models.CharField(max_length=50)
+    country_field = models.CharField(max_length=50)
+    post_field = models.CharField(max_length=50)
+    area_field = models.CharField(max_length=50)
