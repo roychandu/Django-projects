e8745ddffba921be8e64817f30de09aa61a52b55
roychandu <cr717753@gmail.com>
2024-08-29 14:39:56 +0530

    create views and update urls path for inserting data

--- Signup_form/signupform_app/urls.py
+++ Signup_form/signupform_app/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns = [
     path("", views.indexpage, name="frontpage"),
+    path("insert/", views.inserdata, name="insert"),
 ]
--- Signup_form/signupform_app/views.py
+++ Signup_form/signupform_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def indexpage(request):
     return render(request, "app/index.html")
+
+
+def inserdata(request):
+    fname_view = request.POST['fname_input']
+    lname_view = request.POST['lname_input']
+    email_view = request.POST['email_input']
+    password_view = request.POST['password_input']
+
+    newuser = singupdata.objects.create(first_name = fname_view, last_name = lname_view,
+                                         email = email_view, password = password_view)
+    
+    return render(request, "app/show.html")
