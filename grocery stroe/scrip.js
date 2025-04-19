67f505941dc39533ebcbce8f0e0437f14b1ab87f
roychandu <cr717753@gmail.com>
2025-04-19 13:27:07 +0530

    Add captions on silders with animation

--- grocery stroe/scrip.js
+++ grocery stroe/scrip.js
@@ -10,3 +10,35 @@ gsap.to(".navbar02", {
         scrub: true,
     }
 })
+
+function animateActiveSlide() {
+    gsap.from(".carousel-item.active .carousel-caption h1", {
+      y: -100,
+      duration: 0.7,
+      delay: 0.5,
+      opacity: 0,
+      stagger: 0.5,
+      ease: "power2.out"
+    });
+  
+    gsap.from(".carousel-item.active .carousel-caption h2", {
+      x: 200,
+      duration: 0.8,
+      delay: 1,
+      opacity: 0,
+      stagger: 0.5,
+      ease: "power2.out"
+    });
+  }
+  
+  // Animate on initial page load
+  $(document).ready(function () {
+    animateActiveSlide();
+  });
+  
+  // Animate on every slide change
+  $('#carouselExampleIndicators').on('slid.bs.carousel', function () {
+    animateActiveSlide();
+  });
+  
+
